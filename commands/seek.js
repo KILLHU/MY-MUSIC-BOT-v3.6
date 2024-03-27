4ef459b0ba7195eed30c78b52d0db37a4538a67e
KILLHU <95957855+KILLHU@users.noreply.github.com>
2024-03-27 20:44:34 +0700

    seek.js

--- commands/seek.js
+++ commands/seek.js
@@ -2,11 +2,11 @@ const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
   name: "seek",
-  description: "ข้ามไปที่การประทับเวลา",
+  description: "jump to the timestamp",
   permissions: "0x0000000000000800",
   options: [{
     name: "time",
-    description: "ป้อนการประทับเวลา",
+    description: "enter timestamp",
     type: ApplicationCommandOptionType.String,
     required: true
   }],
@@ -15,13 +15,13 @@ module.exports = {
     try {
 
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ไม่พบเพลงที่กำลังเล่น`, ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
 
       let position = getSeconds(interaction.options.getString("position"))
       if(isNaN(position)) return interaction.reply({ content: `usage : 2:40`, ephemeral: true }).catch(e => { })
 
       queue.seek(position)
-      interaction.reply({ content: `▶️ **พาคุณเดินทางข้ามเวลาไปสู่การประทับเวลาที่กำหนด**`}).catch(e => { })
+      interaction.reply({ content: `▶️ **Taking you on a time-travel journey to the specified timestamp.**`}).catch(e => { })
 
     } catch (e) {
       console.error(e);
@@ -33,7 +33,7 @@ function getSeconds(str) {
     if (!str) {
         return 0; 
     }
-    
+
     var p = str.split(':');
     var s = 0;
     var m = 1;
