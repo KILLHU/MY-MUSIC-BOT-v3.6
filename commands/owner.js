f9725c2cd0d3b88b17c19c0a73a4991be086ae9b
KILLHU <95957855+KILLHU@users.noreply.github.com>
2024-03-27 20:32:47 +0700

    owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -18,7 +18,7 @@ const db = require("../mongoDB");
 
 module.exports = {
   name: "owner",
-  description: "รับข้อมูลเกี่ยวกับเจ้าของบอท",
+  description: "Get information about bot owner.",
   permissions: "0x0000000000000800",
   options: [],
 
