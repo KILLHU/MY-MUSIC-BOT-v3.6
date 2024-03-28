9a86f5174c68fb1df94325c89237b09014f6e6a1
KILLHU <95957855+KILLHU@users.noreply.github.com>
2024-03-28 08:28:45 +0700

    volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -4,11 +4,11 @@ const db = require("../mongoDB");
 
 module.exports = {
   name: "volume",
-  description: "Allows you to adjust the music volume.",
+  description: "ช่วยให้คุณปรับระดับเสียงเพลงได้",
   permissions: "0x0000000000000800",
   options: [{
     name: 'volume',
-    description: 'Type the number to adjust the volume.',
+    description: 'พิมพ์ตัวเลขเพื่อปรับระดับเสียง',
     type: ApplicationCommandOptionType.Integer,
     required: true
   }],
@@ -17,20 +17,20 @@ module.exports = {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ไม่พบเพลงที่กำลังเล่น', ephemeral: true });
       }
 
       const vol = parseInt(interaction.options.getInteger('volume'));
 
       if (!vol) {
         return interaction.reply({
-          content: `Current volume: **${queue.volume}** 🔊\nTo change the volume, type a number between \`1\` and \`${maxVol}\`.`,
+          content: `Current volume: **${queue.volume}** 🔊\nหากต้องการเปลี่ยนระดับเสียง ให้พิมพ์ตัวเลขระหว่างn \`1\` and \`${maxVol}\`.`,
           ephemeral: true
         });
       }
 
       if (queue.volume === vol) {
-        return interaction.reply({ content: 'Current volume is already set to **' + vol + '**!', ephemeral: true });
+        return interaction.reply({ content: 'ระดับเสียงปัจจุบันตั้งค่าเป็นแล้ว **' + vol + '**!', ephemeral: true });
       }
 
       if (vol < 1 || vol > maxVol) {
@@ -50,11 +50,11 @@ module.exports = {
         iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157528025739563088/5657-volume-icon.png?ex=6518ef7b&is=65179dfb&hm=1797c2830537a28b5c6a57564517cc509146d02383a69fb4239d7b5d55aceeed&', 
         url: 'https://discord.gg/FUEHs7RCqz'
     })
-          .setDescription(`**Adjusting Volume : ** **${vol}/${maxVol}**`);
+          .setDescription(`**การปรับระดับเสียง : ** **${vol}/${maxVol}**`);
 
         return interaction.reply({ embeds: [embed] });
       } else {
-        return interaction.reply({ content: '❌ Something went wrong while changing the volume.', ephemeral: true });
+        return interaction.reply({ content: '❌เกิดข้อผิดพลาดขณะเปลี่ยนระดับเสียง', ephemeral: true });
       }
     } catch (e) {
       console.error(e);
