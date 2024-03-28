9991409ab3cc4fcc51d2a30718496810bbe6208f
KILLHU <95957855+KILLHU@users.noreply.github.com>
2024-03-28 09:41:28 +0700

    pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -18,7 +18,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "pause",
-  description: "หยุดเล่นเพลงที่กำลังเล่นอยู่",
+  description: "หยุดเล่นเพลงที่กำลังเล่นชั่วคราว",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -39,7 +39,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157296313013117049/8061-purple-pause-icon.png?ex=651817ae&is=6516c62e&hm=4596c9fab9d8b66de8b5215b2750572ced352eed67440a1134550b846b5693b9&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription(success ? '**The music has been Paused for a moment!!**' : '❌ Command Error: Unable to pause song')
+        .setDescription(success ? '**เพลงถูกหยุดชั่วคราว!!**' : '❌ Command Error: Unable to pause song')
 
 
       return interaction.reply({ embeds: [embed] });
