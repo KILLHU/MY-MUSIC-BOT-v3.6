75ee3b066b9da830d8af3c0f2ae0c4086916da87
James <95957855+KILLHU@users.noreply.github.com>
2024-03-31 22:54:01 +0700

    config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   ownerID: ["1004206704994566164", ""],
   botInvite: "",
   supportServer: "",
-  mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
+  mongodbURL: "",
   status: 'RTXX GG',
   commandsDir: './commands',
   language: "en",
