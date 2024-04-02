51337906cda35c20412570cb53cd524e9536bc8f
James <95957855+KILLHU@users.noreply.github.com>
2024-04-02 20:27:38 +0700

    search.js

--- commands/play.js
+++ commands/play.js
@@ -5,7 +5,7 @@ const db = require("../mongoDB");
 let selectedThumbnailURL;
 
 module.exports = {
-  name: "play",
+  name: "search",
   description: "มาฟังเพลงกันเถอะ!",
   permissions: "0x0000000000000800",
   options: [{
