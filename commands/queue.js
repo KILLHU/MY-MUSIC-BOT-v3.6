4dd0ca10144217af84942581c0c0f5325eb9a12d
James <95957855+KILLHU@users.noreply.github.com>
2024-05-15 08:33:02 +0700

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -1,46 +1,31 @@
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const db = require("../mongoDB");
+
 module.exports = {
   name: "queue",
   description: "แสดงรายการคิวเพลง",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
-
     try {
+      const queue = client.player.getQueue(interaction.guild.id);
+      if (!queue || !queue.playing) {
+        return interaction.reply({ content: '⚠️ไม่พบเพลงที่กำลังเล่น', ephemeral: true }).catch(e => { });
+      }
+      if (!queue.songs[0]) {
+        return interaction.reply({ content: '⚠️ คิวว่าง!!', ephemeral: true }).catch(e => { });
+      }
 
-     const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ไม่พบเพลงที่กำลังเล่น', ephemeral: true }).catch(e => { })
-      if (!queue.songs[0]) return interaction.reply({ content: '⚠️ คิวว่าง!!', ephemeral: true }).catch(e => { })
+      const trackl = queue.songs.map((track, i) => ({
+        title: track.name,
+        author: track.uploader.name,
+        user: track.user,
+        url: track.url,
+        duration: track.duration
+      }));
 
-      const trackl = []
-      queue.songs.map(async (track, i) => {
-        trackl.push({
-          title: track.name,
-          author: track.uploader.name,
-          user: track.user,
-          url: track.url,
-          duration: track.duration
-        })
-      })
-
-      const backId = "emojiBack"
-      const forwardId = "emojiForward"
+      const backId = "emojiBack";
+      const forwardId = "emojiForward";
       const backButton = new ButtonBuilder({
         style: ButtonStyle.Secondary,
         emoji: "⬅️",
@@ -59,27 +44,28 @@ module.exports = {
         customId: forwardId
       });
 
-
-      let kaçtane = 8
-      let page = 1
-      let a = trackl.length / kaçtane
+      let kaçtane = 8;
+      let page = 1;
+      let a = trackl.length / kaçtane;
 
       const generateEmbed = async (start) => {
-        let sayı = page === 1 ? 1 : page * kaçtane - kaçtane + 1
-        const current = trackl.slice(start, start + kaçtane)
-        if (!current || !current?.length > 0) return interaction.reply({ content: '⚠️ คิวว่าง!!', ephemeral: true }).catch(e => { })
+        let sayı = page === 1 ? 0 : page * kaçtane - kaçtane;
+        const current = trackl.slice(start, start + kaçtane);
+        if (!current || !current.length > 0) {
+          return interaction.reply({ content: '⚠️ คิวว่าง!!', ephemeral: true }).catch(e => { });
+        }
         return new EmbedBuilder()
-          .setTitle(`${interaction.guild.name}  Queue`)
+          .setTitle(`${interaction.guild.name} Queue`)
           .setThumbnail(interaction.guild.iconURL({ size: 2048, dynamic: true }))
           .setColor(client.config.embedColor)
           .setDescription(`▶️ ตอนนี้กำลังเล่น: \`${queue.songs[0].name}\`
-    ${current.map(data =>
+            ${current.map(data =>
             `\n\`${sayı++}\` | [${data.title}](${data.url}) | (ขอเพลงโดย <@${data.user.id}>)`
-          )}`)
-          .setFooter({ text: `Page ${page}/${Math.floor(a + 1)}` })
+          ).join('')}`)
+          .setFooter({ text: `Page ${page}/${Math.floor(a + 1)}` });
       }
 
-      const canFitOnOnePage = trackl.length <= kaçtane
+      const canFitOnOnePage = trackl.length <= kaçtane;
 
       await interaction.reply({
         embeds: [await generateEmbed(0)],
@@ -88,27 +74,25 @@ module.exports = {
           : [new ActionRowBuilder({ components: [deleteButton, forwardButton] })],
         fetchReply: true
       }).then(async Message => {
-        const filter = i => i.user.id === interaction.user.id
+        const filter = i => i.user.id === interaction.user.id;
         const collector = Message.createMessageComponentCollector({ filter, time: 120000 });
 
-
-        let currentIndex = 0
+        let currentIndex = 0;
         collector.on("collect", async (button) => {
-          if (button?.customId === "close") {
-            collector?.stop()
-           return button?.reply({ content: 'Command Cancelled', ephemeral: true }).catch(e => { })
+          if (button.customId === "close") {
+            collector.stop();
+            return button.reply({ content: 'Command Cancelled', ephemeral: true }).catch(e => { });
           } else {
-
             if (button.customId === backId) {
-              page--
+              page--;
             }
             if (button.customId === forwardId) {
-              page++
+              page++;
             }
 
             button.customId === backId
               ? (currentIndex -= kaçtane)
-              : (currentIndex += kaçtane)
+              : (currentIndex += kaçtane);
 
             await interaction.editReply({
               embeds: [await generateEmbed(currentIndex)],
@@ -121,13 +105,13 @@ module.exports = {
                   ],
                 }),
               ],
-            }).catch(e => { })
-            await button?.deferUpdate().catch(e => { })
+            }).catch(e => { });
+            await button.deferUpdate().catch(e => { });
           }
-        })
+        });
 
-        collector.on("end", async (button) => {
-          button = new ActionRowBuilder().addComponents(
+        collector.on("end", async () => {
+          const disabledButtons = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
               .setStyle(ButtonStyle.Secondary)
               .setEmoji("⬅️")
@@ -142,34 +126,18 @@ module.exports = {
               .setStyle(ButtonStyle.Secondary)
               .setEmoji("➡️")
               .setCustomId(forwardId)
-              .setDisabled(true))
+              .setDisabled(true)
+          );
 
           const embed = new EmbedBuilder()
             .setTitle('Command Timeout')
             .setColor(`#ecfc03`)
-            .setDescription('▶️ ดำเนินการคำสั่งคิวอีกครั้ง!!')
-          return interaction?.editReply({ embeds: [embed], components: [button] }).catch(e => { })
-
-        })
-      }).catch(e => { })
-
+            .setDescription('▶️ ดำเนินการคำสั่งคิวอีกครั้ง!!');
+          return interaction.editReply({ embeds: [embed], components: [disabledButtons] }).catch(e => { });
+        });
+      }).catch(e => { });
     } catch (e) {
-    console.error(e); 
+      console.error(e);
+    }
   }
-  }
-}
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
+};
